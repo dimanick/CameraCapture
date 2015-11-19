c6a4b92534e9a7d4051a197acd125676e81bede2
dimanick <dimanick@gmail.com>
2015-11-19 13:20:32 +0200

    initial

---
+++ CameraCapture/CameraCapture/CameraCapture.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CameraCapture
+{
+    partial class CameraCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CamImageBox0 = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.CamImageBox1 = new Emgu.CV.UI.ImageBox();
+            this.CamImageBox2 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CamImageBox0
+            // 
+            this.CamImageBox0.Location = new System.Drawing.Point(11, 11);
+            this.CamImageBox0.Name = "CamImageBox0";
+            this.CamImageBox0.Size = new System.Drawing.Size(259, 222);
+            this.CamImageBox0.TabIndex = 2;
+            this.CamImageBox0.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(11, 246);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(93, 246);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // CamImageBox1
+            // 
+            this.CamImageBox1.Location = new System.Drawing.Point(276, 12);
+            this.CamImageBox1.Name = "CamImageBox1";
+            this.CamImageBox1.Size = new System.Drawing.Size(259, 222);
+            this.CamImageBox1.TabIndex = 6;
+            this.CamImageBox1.TabStop = false;
+            // 
+            // CamImageBox2
+            // 
+            this.CamImageBox2.Location = new System.Drawing.Point(541, 12);
+            this.CamImageBox2.Name = "CamImageBox2";
+            this.CamImageBox2.Size = new System.Drawing.Size(259, 222);
+            this.CamImageBox2.TabIndex = 7;
+            this.CamImageBox2.TabStop = false;
+            // 
+            // CameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 277);
+            this.Controls.Add(this.CamImageBox2);
+            this.Controls.Add(this.CamImageBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CamImageBox0);
+            this.Name = "CameraCapture";
+            this.Text = "Camera Capture";
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CamImageBox0;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSave;
+        private Emgu.CV.UI.ImageBox CamImageBox1;
+        private Emgu.CV.UI.ImageBox CamImageBox2;
+    }
+}
+
---
+++ CameraCapture/CameraCapture/CameraCapture.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace CameraCapture
+{
+    public partial class CameraCapture : Form
+    {
+        //declaring global variables
+        //private Capture capture;        //takes images from camera as image frames
+        private List<Capture> captures = new List<Capture>();
+        private int cameras = 3;
+        private bool captureInProgress;
+        private bool saveToFile;
+
+        private string saveToPath = @"C:\tmp\";
+        private int frameWidth = 1280;
+        private int frameHeight = 720;
+        private int fps = 1;
+
+        public CameraCapture()
+        {
+            InitializeComponent();
+        }
+        //------------------------------------------------------------------------------//
+        //Process Frame() below is our user defined function in which we will create an EmguCv 
+        //type image called ImageFrame. capture a frame from camera and allocate it to our 
+        //ImageFrame. then show this image in ourEmguCV imageBox
+        //------------------------------------------------------------------------------//
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            var timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            var cameraIndex = 0;
+
+            foreach (var capture in captures)
+            {
+                Mat ImageFrame = capture.QueryFrame();
+                if (cameraIndex == 0) CamImageBox0.Image = ImageFrame;
+                if (cameraIndex == 1) CamImageBox1.Image = ImageFrame;
+                if (cameraIndex == 2) CamImageBox2.Image = ImageFrame;
+                if (saveToFile)
+                {
+                    ImageFrame.Save(String.Concat(saveToPath, timeStamp, "_cam_", cameraIndex.ToString(), ".jpg"));
+                }
+                cameraIndex++;
+            }
+
+            if (saveToFile)
+            {
+                saveToFile = !saveToFile;
+            }
+        }
+
+        //btnStart_Click() function is the one that handles our "Start!" button' click 
+        //event. it creates a new capture object if its not created already. e.g at first time
+        //starting. once the capture is created, it checks if the capture is still in progress,
+        //if so the
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            #region if capture is not created, create it now
+            if (captures.Count == 0)
+            {
+                try
+                {
+                    for (var cameraIndex = 0; cameraIndex < cameras; cameraIndex++)
+                    {
+                        var capture = new Capture(cameraIndex);
+                        capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps, fps);
+                        capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, frameHeight);
+                        capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth, frameWidth);
+
+                        captures.Add(capture);
+                    }
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (captures.Count > 0)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                   // "Start" for resuming capture
+                    btnStart.Text = "Start!"; //
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    btnStart.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (captures.Count > 0)
+            {
+                foreach(var capture in captures)
+                {
+                    capture.Dispose();
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            saveToFile = !saveToFile;
+        }
+    }
+}
